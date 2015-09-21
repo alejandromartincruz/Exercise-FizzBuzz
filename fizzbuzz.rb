9a77f145552344429ea62c643b990c6f6242599e
alejandromartincruz <socorreitor@gmail.com>
2015-09-21 21:59:34 +0200

    added the Iteration 3 part

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,20 +6,37 @@ number = number + 1
 
 # While number is less or equal than 100
 while number <= 100
+
+  #create a string with the number value
+  string_number = number.to_s
+
+  #compare the first character of the stream with 1 and print Bang if true
+  if string_number[0] == "1"
+    print "Bang"
+    if number % 3 != 0
+      print "\n"
+    end
+  end
+
+  #print Fizz if number is divisible by 3
 	if number % 3 == 0
   		print "Fizz"
+
+      #print FizzBuzz if number is divisible by 3 and 5
   		if number % 5 == 0
   			print "Buzz"
   		end
   	print "\n"
 
+  #print Buzz if number is divisible by 5  
 	else
   		if number % 5 == 0
   			puts "Buzz"
   		end
 	end
 	
-	if number % 3 != 0 && number %5 != 0
+  #Print the value of the number if its not divisible by 3 or 5
+	if number % 3 != 0 && number %5 != 0 && string_number[0] != "1"
  		puts number
  	end
 
